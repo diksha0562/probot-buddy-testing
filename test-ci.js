8e06e026bc1650b28d83ee82b8a8ae6a81b999ac
d0a05sx <diksha.agarwal@walmart.com>
2022-04-25 14:39:13 +0530

    merge -1

--- test-ci.js
+++ test-ci.js
@@ -1,4 +1,4 @@
 const assert = require('assert');
 const myFunc = require('./test')
 let a = myFunc();
-assert(Object.keys(a).length===1, "My message goes here");
+assert(Object.keys(a).length===3, "My message goes here");
